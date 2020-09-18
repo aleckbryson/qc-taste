91e4e947c7dd98de412ffe676fe7a196d8fb954f
gkourako <georgekourakos4@gmail.com>
2020-09-18 12:43:12 -0400

    Saved to pull

--- public/js/something.js
+++ public/js/something.js
@@ -1,11 +1,7 @@
 $(document).ready(() => {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
-  $.get("/api/user_data").then(data => {
-    $(".member-name").text(data.username);
-  });
-
   $.get("/api/restaurants/:id").then(data => {
-    $(".vape-god").text(data.name);
+    $(".test").text(data.name);
   });
 });
